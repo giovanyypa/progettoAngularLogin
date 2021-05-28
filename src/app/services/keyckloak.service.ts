6e5861ded6c2c9a4e639faceca922ed6f10b2eb8
ypaflores <giovany.yparraguirre@gmial.com>
2021-05-29 00:13:45 +0200

    metodo refreshtoken aggiunto

--- src/app/services/keyckloak.service.ts
+++ src/app/services/keyckloak.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,27 +7,48 @@ import { Observable } from 'rxjs';
 })
 export class KeyckloakService {
 
-  private keycloakUrl = "http://localhost:8080/auth/realms/Demo-Realm/protocol/openid-connect/token";
-   
-    
+  
+  private keycloakUrl = '/keycloak/auth/realms/realm-login-springboot/protocol/openid-connect/token';
+  private headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+  private client_id = "springboot-microservice";
+  private client_secret = "b6a46747-e77f-40dc-aea6-17431b2b2af8";
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {}
 
 
   // Chiamate HTTP APIKEYCLOAK (POST)
-  public getTokenAccess(username : string,password:string):Observable<any>{
+  public getAccessToken(username : string,password:string):Observable<any>{
 
+    const params = new HttpParams({
+      fromObject: {
+        grant_type: 'password',
+        client_id: this.client_id,
+        username: username,
+        password: password,
+        client_secret:this.client_secret
 
-    let headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+      }
+    });
 
-    let  body = new URLSearchParams();
-      body.set("client_id","springboot-microservice");
-      body.set("client_secret","7d1214f0-57a0-48dc-95b2-97d2616cd388");
-      body.set("username",username);
-      body.set("password",password);
-      body.set("grant_type","password");
-
-    return this.http.post(this.keycloakUrl,body.toString(),{headers: headers});
+    return this.http.post(this.keycloakUrl,params,{headers: this.headers});
 
   }
+
+  public getRefreshAccessToken(refreshToken:string):Observable<any>{
+
+    const params = new HttpParams({
+      fromObject: {
+        grant_type: 'refresh_token',
+        client_id: this.client_id,
+        refresh_token: refreshToken,
+        client_secret:this.client_secret
+
+      }
+    });
+
+    return this.http.post(this.keycloakUrl,params,{headers: this.headers});
+
+  }
+
+
 }
