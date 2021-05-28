a7af692784da7b79a5214e03732d9570e3394e96
ypaflores <giovany.yparraguirre@gmial.com>
2021-05-29 00:15:07 +0200

    login con e senza token

--- src/app/services/users-service.service.ts
+++ src/app/services/users-service.service.ts
@@ -1,54 +1,71 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { DataUs, User } from '../models/iusers';
+import { Observable, of, Subject, throwError } from 'rxjs';
+import { catchError, switchMap, tap } from 'rxjs/operators';
+
+import { DataUs } from '../models/iusers';
 import { KeyckloakService } from './keyckloak.service';
 import { LocalstorageService } from './localstorage.service';
+import { SpringbootService } from './springboot.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersServiceService {
 
-  userTest : User ;
+  private  userInfo = new Subject<DataUs>() ; //beahvior subject ! 
   private _isLoggedIn :boolean = false; 
 
   // store the URL so we can redirect after logging in
   private _redirectUrl: string;
   
-  constructor(private router : Router,private keycloak:KeyckloakService,private storage:LocalstorageService) { 
+  constructor(private router : Router,private keycloak:KeyckloakService,private storage:LocalstorageService , private springBootService:SpringbootService) { 
         
 
   }
 
-  login(username:string,password:string): Observable<Boolean> {
+
+  //res - crea interface interna .
+  login(username:string,password:string): Observable<any> {
     
+    this._isLoggedIn = false;
     
-    this.keycloak.getTokenAccess(username,password)
-        .subscribe((res:any)=>{
+    return this.keycloak.getAccessToken(username,password).pipe(
+      tap( res =>{
           console.log(res);
           this.storage.set("access_token",res.access_token);
           this.storage.set("refresh_token",res.refresh_token); 
           this._isLoggedIn = true;
-        }),
-        (error) => {
-          console.log("not authorization");
-          this._isLoggedIn = false;
-        };
-
-        return of(this._isLoggedIn);
-    
+        },
+      ),switchMap(response=>  {
+          return this.loginWithToken(response.access_token);
+      })
+    ); 
   }
+  
+  loginWithToken(token : string):Observable<DataUs> {
+
+    return this.springBootService.getToUserInfo(token).pipe(
+        tap( resp => {
+          this.userInfo.next(resp);
+        })
+    );
+  }
+
 
   logout(): void {
 
     this._isLoggedIn = false;
-    //this.router.navigate(["/login"]);
+    this.storage.clear();
+
+    this.router.navigate(["/login"]);
 
   }
   
 
-
+  public get userInfoObservable(): Observable<DataUs> {
+    return this.userInfo.asObservable();
+  }
 
   public get isLoggedIn ():boolean{
 
